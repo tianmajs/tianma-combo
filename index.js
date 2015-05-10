101d741a311b344373f148a0b2ebb076c7d706bc
houfeng.hf <houfeng.hf@alibaba-inc.com>
2015-05-10 12:49:33 +0800

    format & ignore

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ var PATTERN_COMBO_URL = /^(\/.*?)\?\?(.*?)(\?.*)?$/;
  * @return {Array|null}
  */
 function parseURL(url) {
-	var re;
-	
-	if (re = url.match(PATTERN_COMBO_URL)) {
-		return re[2].split(',').map(function (pathname) {
-			return re[1] + pathname + (re[3] || '?');
-		});
-	}
+    var re;
 
-	return null;
+    if (re = url.match(PATTERN_COMBO_URL)) {
+        return re[2].split(',').map(function(pathname) {
+            return re[1] + pathname + (re[3] || '?');
+        });
+    }
+
+    return null;
 }
 
 /**
@@ -24,28 +24,27 @@ function parseURL(url) {
  * @param [delimiter] {Object}
  * @return {Function}
  */
-module.exports = function (delimiter) {
-	return function* (next) {
-		var req = this.request,
-			res = this.response,
-			paths,
-			datum = [];
-			
-		if (req.method() === 'GET' && (paths = parseURL(req.path))) {
-			for (let i = 0, len = paths.length; i < len; ++i) {
-				req.url(paths[i]);
-				yield next;
-				if (res.status() !== 200) {
-					res.data(req.pathname);
-					return;
-				}
-				datum.push(res.data());
-			}
+module.exports = function(delimiter) {
+    return function*(next) {
+        var req = this.request,
+            res = this.response,
+            paths,
+            datum = [];
 
-			res.data(datum);
-		} else {
-			yield next;
-		}
-	};
+        if (req.method() === 'GET' && (paths = parseURL(req.path))) {
+            for (let i = 0, len = paths.length; i < len; ++i) {
+                req.url(paths[i]);
+                yield next;
+                if (res.status() !== 200) {
+                    res.data(req.pathname);
+                    return;
+                }
+                datum.push(res.data());
+            }
+
+            res.data(datum);
+        } else {
+            yield next;
+        }
+    };
 };
-
